f2391cf80e0294b5c69b608e8cae3ae89bf88539
EcccsTasyyyyy <lashatutberidze1@gmail.com>
2025-01-15 02:51:08 +0400

    ...

--- Your-Own-Library/Program.cs
+++ Your-Own-Library/Program.cs
@@ -4,18 +4,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.Write($"Enter the maximum amount of book the library can hold: ");
-        int bookCount;
-
-        while (!int.TryParse(Console.ReadLine(), out bookCount) || bookCount <= 0)
-        {
-            Console.WriteLine("Please enter positive integer: ");
-        }
-
-        var library = new Library(bookCount);
-
-        var menu = new LibraryMenu(library);
-
-        menu.ShowLibraryMenu();
+        var libraryMenu = new LibraryMenu();
+        libraryMenu.ShowLibraryMenu();
     }
 }
